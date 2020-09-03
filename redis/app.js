f93071a606ecc21cea04834e24435f0bb3b03508
aakashdeveloper <ahanda205@gmail.com>
2020-09-03 19:44:56 +0530

    A

---
+++ redis/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var axios = require('axios');
+var redis = require('redis');
+const app = express();
+const port = 6600;
+
+const client = redis.createClient({
+    host:'localhost',
+    port:6379
+})
+
+app.get('/data',(req,res) => {
+    const userInput = (req.query.country).trim()
+    const url =`https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=${userInput}`
+
+    return client.get(`wiki:${userInput}`,(err,result) => {
+        if(result){
+            const output = JSON.parse(result);
+            return res.send(output)
+        }else{
+            return axios.get(url)
+                .then(response => {
+                    const output = response.data;
+                    client.setex(`wiki:${userInput}`,3600,JSON.stringify({source:'Redis Cache',output}))
+                    return res.json({source:'Api...',output})
+                })
+        }
+    })
+})
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`)
+})
